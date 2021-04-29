97fca132e2469a653deafe4670f706ef89c2519f
Lacona <azp0136@auburn.edu>
2021-04-29 00:20:03 -0500

    Description of the chunks of the code and made it modular

--- search.py
+++ search.py
@@ -1,25 +1,31 @@
 #! /usr/bin/env python3
 
+import sys
 import re
-#import pdb; pdb.set_trace()
 
-#regex pattern
+#regex pattern to find all words containing 'herit'
 pattern = r'\w*herit\w*'
 
-#gutenberg = open("origin.txt, 'r')
+# results include upper and lower case
 result = re.compile(pattern, re.IGNORECASE)
 
+# list of the results
 list_of_results = []
 
-with open('origin.txt', 'r') as gutenberg:
-    with open('matches.txt', 'w') as output:
-        index = 0
-        for line in gutenberg:
-            line = line.strip()
-            list = line.split()
-            index += 1
-            matches = result.search(line)
-            if matches:
-               output.write(str(index)+"\t"+matches.group(0)+"\n")
+# open the gutenberg text and write the results in the output file matches.txt
+def matches():
+    with open('origin.txt', 'r') as gutenberg:
+        with open('matches.txt', 'w') as output:
+            index = 0
+    
+# adding the line numbers and layout of the output
+            for line in gutenberg:
+                line = line.strip()
+                list = line.split()
+                index += 1
+                matches = result.search(line)
+                if matches:
+                   output.write(str(index)+"\t"+matches.group(0)+"\n")
 
-
+if __name__ == '__main__':
+    main()
